62e89edcd312aad7a8b6945f99ed224fc7987876
Nathaniel Meyer <nathaniel.meyer@gatech.edu>
2015-08-02 15:45:41 -0400

    Add dropchest command

--- src/main/java/io/github/samurainate/chestdrop/ChestDropPlugin.java
+++ src/main/java/io/github/samurainate/chestdrop/ChestDropPlugin.java
@@ -2,11 +2,15 @@ package io.github.samurainate.chestdrop;
 
 import java.util.logging.Logger;
 
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class ChestDropPlugin extends JavaPlugin {
 
 	private Logger logger;
+	private PluginConfig pluginConfig;
 
 	@Override
 	public void onDisable() {
@@ -20,19 +24,30 @@ public class ChestDropPlugin extends JavaPlugin {
 		logger = getServer().getLogger();
 
 		/* Load plugin config */
-		PluginConfig config = new PluginConfig(this);
+		PluginConfig pluginConfig = new PluginConfig(this);
+		this.pluginConfig = pluginConfig;
 
 		/* Announce WorldBorder integration */
-		if (config.isWorldBorderEnabled())
+		if (pluginConfig.isWorldBorderEnabled())
 			logger.info("[ChestDrop] WorldBorder integration enabled");
 
 		/* Schedule drops */
-		Utils.scheduleTasks(config);
+		Utils.scheduleTasks(pluginConfig);
 		
 		/* Announce ready */
 		getServer().getLogger().info("[ChestDrop] Ready");
 
 	}
-
+	
+	@Override
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+		if (cmd.getName().equalsIgnoreCase("dropchest")) { 
+			if (sender instanceof Player && sender.hasPermission("chestdrop.dropchest")) {
+				Utils.dropChest(pluginConfig, ((Player)sender).getWorld().getName());
+				return true;
+			}
+		} 
+		return false; 
+	}
 
 }
--- src/main/java/io/github/samurainate/chestdrop/Utils.java
+++ src/main/java/io/github/samurainate/chestdrop/Utils.java
@@ -1,9 +1,16 @@
 package io.github.samurainate.chestdrop;
 
+import org.bukkit.Chunk;
 import org.bukkit.Location;
+import org.bukkit.Material;
 import org.bukkit.Server;
 import org.bukkit.World;
+import org.bukkit.block.Block;
+import org.bukkit.block.Chest;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 
 public class Utils {
 
@@ -67,4 +74,92 @@ public class Utils {
 					new ChestDropTask(config, worldname), worldConfig.getDropInterval(), worldConfig.getDropInterval());
 		}
 	}
+	
+	public static boolean dropChest(PluginConfig config, String worldname) {
+		/* Get random drop coords */
+		double[] coords = Utils.getDrop(config, worldname);
+		
+		World world = config.getServer().getWorld(worldname);
+		if(world==null) {
+			return false;
+		}
+
+		/* Load drop chunk */
+		loadChunk(world, coords);
+
+		/* Drop to ground loop */
+		Block block = null;
+		int y;
+		for (y = world.getMaxHeight(); y > 0; y--) {
+			block = world.getBlockAt((int) coords[0], y - 1, (int) coords[1]);
+			switch (block.getType()) {
+			/* fall through air, water, leaves */
+			case AIR:
+			case WATER:
+			case STATIONARY_WATER:
+			case LEAVES:
+				continue;
+			/* shift so we don't land on or in trees */
+			case LOG:
+			case LOG_2:
+				boolean axis = config.getRandom().nextBoolean();
+				boolean direction = config.getRandom().nextBoolean();
+				int a=axis?0:1;
+				int d=direction?1:-1;
+				/* move one */
+				coords[a]+=d;
+				/* start over */
+				loadChunk(world,coords);
+				y=world.getMaxHeight()+1;
+				continue;
+			case LAVA:
+			case STATIONARY_LAVA:
+				/* better luck next time */
+				return false;
+			default:
+				break;
+			}
+			break;
+		}
+		if (y == 0) {
+			config.getServer().getLogger().info("[ChestDrop] chest fell into the void...");
+			return false; // Abort if we make it to the void
+		}
+
+		/* Place Chest with Marker */
+		block = world.getBlockAt((int) coords[0], y+3, (int) coords[1]);
+		block.setType(Material.GLOWSTONE);
+		block = world.getBlockAt((int) coords[0], y+2, (int) coords[1]);
+		block.setType(Material.FENCE);
+		block = world.getBlockAt((int) coords[0], y+1, (int) coords[1]);
+		block.setType(Material.FENCE);
+		block = world.getBlockAt((int) coords[0], y, (int) coords[1]);
+		block.setType(Material.CHEST);
+
+		/* Open Chest */
+		Chest chest = (Chest) block.getState();
+		Inventory in = chest.getBlockInventory();
+
+		/* Generate Treasure */
+		ItemStack gem = new ItemStack(Material.EMERALD, 1 + config.getRandom().nextInt(5));
+		ItemMeta meta = gem.getItemMeta();
+		meta.setDisplayName("Hidden Gem");
+		gem.setItemMeta(meta);
+
+		/* Put Treasure in Chest */
+		in.setItem(in.firstEmpty(), gem);
+
+		/* Notify players */
+		Utils.worldBroadcast(config.getServer(), worldname,
+				String.format("Chest dropped at %1.0fX, %1.0fZ", coords[0], coords[1]));
+		return true;
+	}
+	
+	public static void loadChunk(World world, double[] coords) {
+		Chunk chunk = world.getChunkAt((int) coords[0], (int) coords[1]);
+		if (!chunk.isLoaded()) {
+			chunk.load(true);
+		}
+	}
+
 }
